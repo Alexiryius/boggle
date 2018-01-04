7f2c175f81a6e9635fc07a3daf273f5d5074a47d
alexi <alexi@DESKTOP-9I4900T>
2018-01-04 18:52:50 +0100

    oups oublié mon ptit class de test

--- sources/boggle/mots/Test.java
+++ sources/boggle/mots/Test.java
@@ -6,8 +6,17 @@ public class Test {
 	public static void main(String[] args) {
 		ArbreLexical a = new ArbreLexical();
 		a.lireMots("config/dict-fr.txt") ;
+//a.ajouterNouveau("BONJOUR");
+//a.ajouterNouveau("SALUT");
+//a.ajouterNouveau("TYNJOUR");
+//a.ajouterNouveau("BONJALLILO");
+//a.ajouterNouveau("BONJOUR");
+//a.ajouterNouveau("BONJ");a.ajouterNouveau("BONJAUL");
+//a.toStrong();
 
-//		System.out.println(a.verifierMot("Iʳ"));
+		System.out.println("réponse : "+a.contient("OEUVRAMESIMO"));
+
+
 		
 	}
 
